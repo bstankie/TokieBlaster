215c29f63d2467f81f22c7313738fd178028f55b
bstankie <bstankie@gmail.com>
2021-03-23 07:32:16 -0500

    Modify TokieBlaster.py trying to get switch callback logic.

--- TokieBlaster.py
+++ TokieBlaster.py
@@ -3,6 +3,9 @@
 import time
 from grove.gpio import GPIO
 from grove.factory import Factory
+from grove.button import Button
+from grove.grove_ryb_led_button import GroveLedButton
+
 
 __all__ = ["GroveRelay"]
 
@@ -30,21 +33,37 @@ class GroveRelay(GPIO):
 
 
 Grove = GroveRelay
+
+button = GroveLedButton(5)
+
+def on_event(index, event, tm):
+    print("event index:{} ".format(index))
+    print("event:{}".format(event))
+    #print("pressed:{}".format(event['presesed']))
+    if event & Button.EV_SINGLE_CLICK:
+        print('single click')
+        button.led.light(True)
+        return True
+    elif event & Button.EV_LONG_PRESS:
+        print('long press')        if State == 'Running':
+
+        button.led.light(False)
+        return False
+button.on_event = on_event
+
+
 def InitializeDisplay(lcd,totalTime):
     # LCD 16x2 Characters
     lcd.setCursor(0, 0)
     lcd.write("Tokie Blaster")
-    lcd.setCursor(1, -5)
+    lcd.setCursor(1, 0)
     lcd.write("Ready")
     mins, secs = divmod(totalTime, 60)
-    lcd.setCursor(1, 0)
-
-    lcd.write('T:{:02d}:{:02d}'.format(mins, secs)) 
-
-def Countdown(lcd,totalTime):
+    lcd.setCursor(1, -5)
+    lcd.write('T:{:02d}:{:02d}'.format(mins, secs))
+    
+def Countdown(lcd,relay,totalTime):
     currTime = totalTime
-    pin = 12
-    relay = GroveRelay(pin)
     relay.on()
     while currTime:
         try:
@@ -58,13 +77,23 @@ def Countdown(lcd,totalTime):
             lcd.setCursor(1, 0)
             relay.off()
             lcd.write("ABORTED!")
-            exit(1)            
+            exit(1)
+    lcd.setCursor(1,0)
+    #mins, secs = divmod(totalTime, 60)
+    #lcd.write('DONE {:02d}:{:02d}'.format(mins, secs))
     relay.off()
 if __name__ == '__main__':
     lcd = Factory.getDisplay("JHD1802")
-    totalTime = 30
-    InitializeDisplay(lcd,totalTime)
-    Countdown(lcd,totalTime)
+    relayPin = 12
+    relay = GroveRelay(relayPin)
+    totalTime = 5
+    relayState=0
+    #ledButton = GroveLedButton(5)
+
+    while True:
+        InitializeDisplay(lcd,totalTime)
+        if State == 'Running':
+            Countdown(lcd,relay,totalTime)
     #main()
 
 
